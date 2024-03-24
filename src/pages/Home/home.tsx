77d85ad3b296713cb94d3000da1474cdeea09aa7
gabrielynw <gabrielsousadeveloper@gmail.com>
2024-03-24 19:03:13 -0300

    feat: criando e estilizando componente ourcoffees

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,10 +1,12 @@
 import { Intro } from './components/Intro/intro';
+import { OurCoffees } from './components/OurCoffes/our-coffees';
 import { HomeContainer } from './styles';
 
 export function HomePage() {
   return (
     <HomeContainer>
       <Intro />
+      <OurCoffees />
     </HomeContainer>
   );
 }
