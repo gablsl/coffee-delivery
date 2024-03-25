cc52a098624dbe0618e4f7ea3365bdb1d88c3c99
gabrielynw <gabrielsousadeveloper@gmail.com>
2024-03-25 10:58:59 -0300

    feat: estilos do form

---
+++ src/pages/CompleteOrder/componets/CompleteOrderForm/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+import { SectionBaseStyle } from '../../styles';
+
+export const CompleteOrderFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  width: 640px;
+`;
+
+export const FormSectionContainer = styled(SectionBaseStyle)`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const AddressFormContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 200px 276px 60px;
+  column-gap: 12px;
+  row-gap: 16px;
+  grid-auto-flow: dense;
+
+  .cep {
+    grid-column: span 3;
+    max-width: 200px;
+  }
+
+  .street {
+    grid-column: span 3;
+  }
+
+  .complement {
+    grid-column: span 2;
+  }
+`;
+
+export const PaymentOptionsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 12px;
+`;
