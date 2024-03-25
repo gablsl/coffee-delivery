a29717f64584237a163972934f0f34b25f778aff
gabrielynw <gabrielsousadeveloper@gmail.com>
2024-03-25 14:50:53 -0300

    fix: ajustando erro de ref no componente input

--- src/pages/CompleteOrder/componets/PaymentMethodInput/payment-method-input.tsx
+++ src/pages/CompleteOrder/componets/PaymentMethodInput/payment-method-input.tsx
@@ -1,11 +1,24 @@
-import { CreditCard } from 'phosphor-react';
-import { PaymentoMethodInputContainer } from './styles';
+import { ContentContainer, PaymentoMethodInputContainer } from './styles';
+import { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
 
-export function PaymentoMethodInput() {
+type PaymentoMethodInputProps = InputHTMLAttributes<HTMLInputElement> & {
+  icon: ReactNode;
+  label: string;
+};
+
+export const PaymentMethodInput = forwardRef<
+  HTMLInputElement,
+  PaymentoMethodInputProps
+>(({ id, icon, label, ...props }, ref) => {
   return (
     <PaymentoMethodInputContainer>
-      <CreditCard size={16} />
-      Cartão de crédito
+      <input id={id} type='radio' {...props} name='paymentMethod' ref={ref} />
+      <label htmlFor={id}>
+        <ContentContainer>
+          {icon}
+          {label}
+        </ContentContainer>
+      </label>
     </PaymentoMethodInputContainer>
   );
-}
+});
